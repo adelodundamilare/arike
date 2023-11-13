2bc24a58c889666be062a6716e0d3162743f3a9e
Adelodun Damilare <adelodundamilare@gmail.com>
2023-11-13 06:02:23 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				primary: '#aaa',
+				secondary: '#777',
+				accent: '#e7f870'
+			},
+			fontSize: {
+				sm: '12px'
+			},
+			screens: {
+				sm: { max: '639px' },
+				md: { max: '767px' },
+				lg: { max: '1023px' },
+				xl: { max: '1279px' }
+			}
+		}
+	},
+	variants: {
+		translate: ['group-hover'],
+		textColor: ['group-hover']
+		// extends: {
+		// },
+	},
+	plugins: []
+};
